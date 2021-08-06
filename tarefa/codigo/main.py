06dc9a831cb8667d2bf545c9c5f407b0e79ab2d9
vitorrangelcs <vitorrangelcs@gmail.com>
2021-08-06 14:25:12 -0300

    Tarefa+compose+dockerfile

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -4,9 +4,23 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+
+async def titanic(Sex:int,Age:float, Lifeboat:int, Pclass:int):
     with open('model/Titanic.pkl', 'rb') as fid: 
-        titanic = pickle.load(fid)
+        titanic = pickle.load(fid)    
+    
+    predi = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()
+
+    try:
+        return {
+            'survived': predi,
+            'status': 200,
+            'message': 'Deu tudo certo \o',
+        }
+    except Exception:
+        return {
+            'message': 'Estou com algum problema :('
+        }
 
 @app.get('/model')
 def get():
